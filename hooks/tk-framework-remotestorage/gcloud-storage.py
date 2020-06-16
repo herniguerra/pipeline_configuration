ad300dc8be468b200f9b3940c51977f8cdbdaa0d
herniguerra <hernan.g@many-worlds.com>
2020-06-16 20:26:34 -0300

    Revert "Update gcloud-storage.py"
    This reverts commit 052d7056954c3acfd95d0ca253e1af19eb622947.

--- hooks/tk-framework-remotestorage/gcloud-storage.py
+++ hooks/tk-framework-remotestorage/gcloud-storage.py
@@ -11,8 +11,29 @@
 """
 Hook that provides upload and download functionality for the cloud storage provider.
 """
+
 import os
 import sgtk
+import subprocess
+
+sgtk.util.append_path_to_env_var(
+    "Path", "C:/Users/hernan.g/AppData/Local/Google/Cloud SDK/google-cloud-sdk/bin"
+)
+
+sgtk.util.append_path_to_env_var(
+    "Path",
+    "c:/users/hernan.g/appdata/local/programs/python/python36/lib/site-packages/",
+)
+
+
+from google.cloud import storage
+
+os.environ[
+    "GOOGLE_APPLICATION_CREDENTIALS"
+] = "M:/offline/key/many-worlds-d48d0252c0e6.json"
+
+bucket_name = "manyworlds-test-bucket"
+
 
 HookBaseClass = sgtk.get_hook_baseclass()
 
@@ -23,8 +44,6 @@ class LocalProvider(HookBaseClass):
     This hooks is just an example hook that doesn't actually upload anything,
     but instead copies thee files to a location, and then retrieves them on download.
     """
-
-    remote_storage_location = "$HOME/mock_remote_storage"
 
     def upload(self, published_file):
         """
@@ -45,16 +64,30 @@ class LocalProvider(HookBaseClass):
             # Build a path to copy the published file to in our mocked remote storage.
             destination_path = self._generate_remote_path(published_file)
             self.logger.info("mock uploading file to %s" % destination_path)
+
+<<<<<<< HEAD
             if os.path.exists(destination_path):
+=======
+            storage_client = storage.Client()
+            bucket = storage_client.get_bucket(bucket_name)
+            blob = bucket.blob(destination_path)
+
+            if blob.exists():
+>>>>>>> parent of 67bd31f... Update gcloud-storage.py
                 self.logger.warning(
                     "PublishedFile already exists in remote location: %s"
                     % published_file
                 )
                 return
 
+            """
             sgtk.util.filesystem.copy_file(
                 published_file["path"]["local_path"], destination_path
             )
+            """
+
+            blob.upload_from_filename(published_file["path"]["local_path"])
+
             return destination_path
 
         self.logger.warning(
@@ -90,7 +123,20 @@ class LocalProvider(HookBaseClass):
             )
             return
 
+        """
         sgtk.util.filesystem.copy_file(remote_path, destination)
+        """
+
+<<<<<<< HEAD
+        cmd = ("gsutil cp " + remote_path, destination)
+        p = subprocess.Popen(cmd)
+=======
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(remote_path)
+        blob.download_to_filename(destination)
+>>>>>>> parent of 67bd31f... Update gcloud-storage.py
+
         return destination
 
     def _generate_remote_path(self, published_file):
@@ -104,4 +150,5 @@ class LocalProvider(HookBaseClass):
         file_name = "{id}_{name}".format(
             id=published_file["id"], name=published_file["name"]
         )
-        return os.path.join(os.path.expandvars(self.remote_storage_location), file_name)
+
+        return file_name
