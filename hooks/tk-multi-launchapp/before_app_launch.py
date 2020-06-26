cdae9da81861c1a7ef3615fa70c352ded03b66fe
herniguerra <hernan.g@many-worlds.com>
2020-06-25 21:02:23 -0300

    shelf work

--- hooks/tk-multi-launchapp/before_app_launch.py
+++ hooks/tk-multi-launchapp/before_app_launch.py
@@ -46,9 +46,12 @@ class BeforeAppLaunch(sgtk.Hook):
 
         if engine_name == "tk-maya":
             sys.path.append("C:/Many-Worlds/rigging/mw_rig_dev/mw_facial/")
+            sys.path.append(os.path.join(self.disk_location, "maya/prefs")
+            '''
             sgtk.util.append_path_to_env_var(
                 "MAYA_APP_DIR", os.path.join(self.disk_location, "maya/prefs")
             )
+            '''
             sgtk.util.append_path_to_env_var(
                 "PYTHONPATH", os.path.join(self.disk_location, "maya")
             )
